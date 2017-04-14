6ace2d3b0154725adc33e00ae387617b2cc704e2
Fernando Salazar <fernando.jsr5@gmail.com>
2017-04-13 20:44:53 -0400

    Updating Home Page and Debug Validations

--- client/views/home.js
+++ client/views/home.js
@@ -1,11 +1,8 @@
 Template.home.onRendered(function(){
-	$(document).ready(function(){
-    $('.parallax').parallax();
-  });
   $(document).ready(function(){
     $('.materialboxed').materialbox();
   });
   $(document).ready(function(){
-    $('.carousel').carousel();
-  });
+	  $('.slider').slider();
+	});
 });
